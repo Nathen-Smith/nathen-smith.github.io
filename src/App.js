4df95c2636ce1a961f2b58c9a3b1321e312c433b
Nathen Smith <ncsmith2@illinois,edu>
2021-05-31 04:09:14 -0500

    transition on scroll frame done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import React from 'react';
+import TrackVisibility from 'react-on-screen';
 import './scss/style.scss';
 
-function App() {
+var show = false;
+
+const ComponentToTrack = ({ isVisible }) => {
+  var style = {}
+  if (isVisible && !show) {
+    style = {background: 'red', transition: '0.4s', width: '100px'};
+    show = true;
+  } else if (show) {
+    style = {background: 'red', width: '100px'};
+  } else if (!isVisible){
+    style = {background: 'blue', width: '50px'};
+  }
+
+  return <div className="skillbar" style={style}>Hello</div>;
+}
+
+const App = () => {
   return (
     <div>
       Hello
-      <h2>pls</h2>
-
+      <h2 style={{height:"1000px"}}>pls</h2>
+      <TrackVisibility>
+        <ComponentToTrack />
+      </TrackVisibility>
+      <h2 style={{height:"100px"}}>pls</h2>
     </div>
 
   );
