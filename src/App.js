6895ca25191ab1e771e01566de3eeb2b482ab9a4
Nathen Smith <ncsmith2@illinois,edu>
2021-05-31 18:54:07 -0500

    basic skill bar supporting  mobile viewing

--- src/App.js
+++ src/App.js
@@ -1,29 +1,53 @@
 import React from 'react';
 import TrackVisibility from 'react-on-screen';
 import './scss/style.scss';
+import useScreenType from "react-screentype-hook";
 
-var show = false;
+const values = [
+  {
+    text: "JavaScript",
+    rating: "Intermediate",
+    before: {width: '0%', background: 'red'},
+    active: {transition: '3s', background: 'red', width: '80%' }
+  },
+  {
+    text: "two",
+    css: { color: 'white' },
+    active: { color: 'blue', transition: '1s' }
+    // active: styles.active
+  },
+];
+// var show = false;
 
 const ComponentToTrack = ({ isVisible }) => {
-  var style = {}
-  if (isVisible && !show) {
-    style = {background: 'red', transition: '0.4s', width: '100px'};
-    show = true;
-  } else if (show) {
-    style = {background: 'red', width: '100px'};
-  } else if (!isVisible){
-    style = {background: 'blue', width: '50px'};
-  }
+  // var style = {}
+  const screenType = useScreenType();
 
-  return <div className="skillbar" style={style}>Hello</div>;
+  return (
+    
+    <div>{values.map(
+      (value, i) => 
+      <div className="skillbar" style={screenType.isMobile ? {width:'100%'} : {width:'20%'}}>
+        <div key={i} className="skillbar-title" style={isVisible ? value.active : value.before}>
+          <span>
+            {value.text}
+          </span>
+        </div>
+        <div className="skill-bar-percent" style={isVisible ? {transition:'3s linear',color:'black'} : {color:'white'}}>
+            {value.rating}
+        </div>
+      </div>
+
+    )}</div>
+  ) 
 }
 
 const App = () => {
   return (
-    <div>
+    <div style={{background:'#292c33'}}>
       Hello
       <h2 style={{height:"1000px"}}>pls</h2>
-      <TrackVisibility>
+      <TrackVisibility once>
         <ComponentToTrack />
       </TrackVisibility>
       <h2 style={{height:"100px"}}>pls</h2>
