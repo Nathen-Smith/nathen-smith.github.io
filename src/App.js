450cd700439b2151f042478101b42a0a08385a48
Nathen Smith <ncsmith2@illinois.edu>
2021-06-01 22:00:10 -0500

    fixed formatting of links

--- src/App.js
+++ src/App.js
@@ -16,7 +16,6 @@ const App = () => {
       <Links />
       <br/>
     </div>
-
   );
 }
 
--- src/components/Links.js
+++ src/components/Links.js
@@ -4,33 +4,47 @@ import fontawesome from '@fortawesome/fontawesome'
 import brands from '@fortawesome/fontawesome-free-brands'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { faEnvelope } from '@fortawesome/free-regular-svg-icons'
 import './links.css';
 
 fontawesome.library.add(brands)
 
+const links = [
+  {
+    link: "https://github.com/Nathen-Smith",
+    icon: faGithub
+  },
+  {
+    link: "https://www.linkedin.com/in/nathen-s-324378141",
+    icon: faLinkedin
+  },
+  {
+    link: "mailto:nathencsmith@gmail.com",
+    icon: faEnvelope
+  }
+]
+
 const Links = () => {
   // const screenType = useScreenType();
-  return (
+  var views = []
+  links.forEach((link) => {
+    views.push(
+      <div>
       <TrackVisibility once>
         {({isVisible}) => {
           return (
-            <div>
-              <a href="https://github.com/Nathen-Smith">
-                <FontAwesomeIcon icon={faGithub}
+            <a href={link.link}>
+              <FontAwesomeIcon icon={link.icon}
                 style={isVisible ? {transform: 'scale(1)',animation: 'bounceIn 0.7s linear 1',animationDelay: '0.1s',color: 'white', width:'80px', height: '80px'} : {color: 'white', width:'80px', height: '80px'}}
-                />
-              </a>
-              <a href="https://www.linkedin.com/in/nathen-s-324378141/">
-                <FontAwesomeIcon icon={faLinkedin}
-                style={isVisible ? {transform: 'scale(1)',animation: 'bounceIn 0.7s linear 1',animationDelay: '0.1s',color: 'white', width:'80px', height: '80px'} : {color: 'white', width:'80px', height: '80px'}}
-                />
-              </a>
-              
-            </div>
+              />
+            </a>
           )
         }}
       </TrackVisibility>
-  )
+      </div>
+    );
+  });
+  return views;
  
 }
 
