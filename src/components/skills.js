410269730914e9d90f7b4a8cddab7e629b751710
Nathen Smith <ncsmith2@illinois.edu>
2021-06-03 03:44:15 -0500

    skills edited, unnecessary code removed

--- src/components/skills.js
+++ src/components/skills.js
@@ -1,55 +1,47 @@
 const values = [
   {
     text: "JavaScript",
-    rating: "Intermediate",
     before: {width: '0%', background: '#F0DB4F'},
-    active: {transition: '3s', background: '#F0DB4F', width: '60%' }
-  },
-  {
-    text: "Linux",
-    rating: "Intermediate",
-    before: {width: '0%', background: '#0040AD'},
-    active: {transition: '3s', background: '#0040AD', width: '70%' }
-  },
-  {
-    text: "Go",
-    rating: "Intermediate",
-    before: {width: '0%', background: '#29BEB0'},
-    active: {transition: '3s', background: '#29BEB0', width: '40%' }
+    active: {transition: '3s', background: '#F0DB4F', width: '75%' }
   },
   {
     text: "Python",
-    rating: "Intermediate",
-    before: {width: '0%', background: '#4B8BBE'},
-    active: {transition: '3s', background: '#4B8BBE', width: '65%' }
+    before: {width: '0%', background: '#ff9800'},
+    active: {transition: '3s', background: '#ff9800', width: '70%' }
   },
+  {
+    text: "Linux",
+    before: {width: '0%', background: '#A1A1A4'},
+    active: {transition: '3s', background: '#A1A1A4', width: '40%' }
+  },
+  {
+    text: "Go",
+    before: {width: '0%', background: '#29BEB0'},
+    active: {transition: '3s', background: '#29BEB0', width: '60%' }
+  },
+  
   {
     text: "React",
-    rating: "Intermediate",
     before: {width: '0%', background: '#61DBFB'},
-    active: {transition: '3s', background: '#61DBFB', width: '70%' }
+    active: {transition: '3s', background: '#61DBFB', width: '80%' }
   },
   {
-    text: "SQL (MySQL)",
-    rating: "Pro",
-    before: {width: '0%', background: '#F29111'},
-    active: {transition: '3s', background: '#F29111', width: '85%' }
+    text: "SQL",
+    before: {width: '0%', background: '#00758F'},
+    active: {transition: '3s', background: '#00758F', width: '90%' }
   },
   {
-    text: "MongoDB",
-    rating: "Intermediate",
+    text: "NOSQL",
     before: {width: '0%', background: '#4DB33D'},
     active: {transition: '3s', background: '#4DB33D', width: '55%' }
   },
   {
     text: "C++",
-    rating: "Pro",
     before: {width: '0%', background: '#5E97D0'},
     active: {transition: '3s', background: '#5E97D0', width: '75%' }
   },
   {
-    text: "Github & Gitlab",
-    rating: "Pro",
+    text: "Github",
     before: {width: '0%', background: '#e24329'},
     active: {transition: '3s', background: '#e24329', width: '80%' }
   },
