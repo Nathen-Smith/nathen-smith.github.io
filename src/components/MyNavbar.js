30d06c26a1662948efd0094597f0faaeacd2f468
Nathen Smith <ncsmith2@illinois.edu>
2021-06-04 01:28:42 -0700

    navbar styling

--- src/components/MyNavbar.js
+++ src/components/MyNavbar.js
@@ -3,33 +3,33 @@ import Nav from 'react-bootstrap/Nav';
 import { NavHashLink } from 'react-router-hash-link';
 import { faGithub, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import useScreenType from '../shared-hooks/useScreenType'
+// import useScreenType from '../shared-hooks/useScreenType'
 import '../scss/style.scss'
 
 const MyNavbar = () => {
-  const screenType = useScreenType();
-  if (screenType === "mobile") {
-    <Navbar collapseOnSelect bg="light" expand="md" sticky="top">
-      <NavHashLink smooth to="#">
-        <Navbar.Brand className="nathen">
-            Nathen Smith
-        </Navbar.Brand>
-      </NavHashLink>
-      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-      <Navbar.Collapse id="responsive-navbar-nav">
-      <Nav className="mr-auto">
-        {/* <NavHashLink smooth to="#about">
-          <Navbar.Brand className="font-weight-bold">
-              About
-          </Navbar.Brand>
-        </NavHashLink> */}
-        <Nav.Link href="#about">About</Nav.Link>
-        <Nav.Link href="#Portfolio">Portfolio</Nav.Link>
-        <Nav.Link href="#contact">Contact</Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
-  }
+  // const screenType = useScreenType();
+  // if (screenType === "mobile") {
+  //   <Navbar collapseOnSelect bg="light" expand="md" sticky="top">
+  //     <NavHashLink smooth to="#">
+  //       <Navbar.Brand className="nathen">
+  //           Nathen Smith
+  //       </Navbar.Brand>
+  //     </NavHashLink>
+  //     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  //     <Navbar.Collapse id="responsive-navbar-nav">
+  //     <Nav className="mr-auto">
+  //       {/* <NavHashLink smooth to="#about">
+  //         <Navbar.Brand className="font-weight-bold">
+  //             About
+  //         </Navbar.Brand>
+  //       </NavHashLink> */}
+  //       <Nav.Link href="#about">About</Nav.Link>
+  //       <Nav.Link href="#Portfolio">Portfolio</Nav.Link>
+  //       <Nav.Link href="#contact">Contact</Nav.Link>
+  //       </Nav>
+  //     </Navbar.Collapse>
+  //   </Navbar>
+  // }
   return (
     <Navbar collapseOnSelect bg="light" expand="md" sticky="top">
       <NavHashLink smooth to="#">
@@ -53,12 +53,10 @@ const MyNavbar = () => {
         <Nav.Link href="#portfolio">Portfolio</Nav.Link>
         <Nav.Link href="#contact">Contact</Nav.Link>
           <Nav.Link href="https://github.com/Nathen-Smith">
-            <FontAwesomeIcon icon={faGithub} size="lg">
-            </FontAwesomeIcon>
+            <FontAwesomeIcon icon={faGithub} size="lg" className="navlink"></FontAwesomeIcon>
           </Nav.Link>
           <Nav.Link href="https://www.linkedin.com/in/nathen-s-324378141">
-            <FontAwesomeIcon icon={faLinkedinIn} size="lg">
-            </FontAwesomeIcon>
+            <FontAwesomeIcon icon={faLinkedinIn} size="lg" className="navlink"></FontAwesomeIcon>
           </Nav.Link>
         </Nav>
         
