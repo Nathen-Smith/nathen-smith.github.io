e6ba06d99b5c5512e76946283e4c7f31f691aaa2
Nathen Smith <ncsmith2@illinois.edu>
2021-06-07 15:50:23 -0700

    changed animation trigger of skills, fixes mobile choppiness somewhat

--- src/components/ShowSkills.js
+++ src/components/ShowSkills.js
@@ -5,22 +5,23 @@ import { Container } from 'react-bootstrap';
 import { InView } from 'react-intersection-observer';
 
 const ShowSkills = () => {
-  return (<div>{
-  values.map((value, i) =>
-    <InView triggerOnce={true} key={i}>
+  return (
+    <InView triggerOnce={true}>
       {({ ref, inView }) => (
         <Container ref={ref}>
-          <div className="skillbar">
-            <div className="skillbar-title" style={inView ? value.active : value.before}>
-              <span>
-                {value.text}
-              </span>
+          {values.map((value, i) =>
+            <div key={i} className="skillbar">
+              <div className="skillbar-title" style={inView ? value.active : value.before}>
+                <span>
+                  {value.text}
+                </span>
+              </div>
             </div>
-          </div>
+          )}
         </Container>
       )}
     </InView>
-  )}</div>);
+  );
 }
 
 export default ShowSkills;
